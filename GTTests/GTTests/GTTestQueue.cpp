40a6a099127737768ed409feb0cd04ab173d3b9d
SnehaAgarwal <v-snagarwal@microsoft.com>
2021-09-28 14:12:47 -0700

    Queue tests complete

--- GTTests/GTTests/GTTestQueue.cpp
+++ GTTests/GTTests/GTTestQueue.cpp
@@ -4,59 +4,110 @@
 
 TEST(QueueTests, TestIfQueueIsEmpty) {
 	//set
-	//rear = 0;
-	//font = 0;
+	Queue* q = new Queue();
+
 	//act
-	//isEmpty();
+	q->isEmpty();
 
 	//assert
+	ASSERT_TRUE(true);
+}
 
+TEST(QueueTests, TestIfQueueIsNotEmpty) {
+	//set
+	Queue* q = new Queue();
+	q->enqueue(1);
+
+	//act
+	q->isEmpty();
+
+	//assert
+	ASSERT_FALSE(false);
 }
 
 
 TEST(QueueTests, TestIfQueueIsFull) {
-	//set
+	Queue* q = new Queue();
+	q->enqueue(2);
+	q->enqueue(3);
+	q->enqueue(4);
+	q->enqueue(5);
+	q->enqueue(6);
 
 	//act
+	q->isFull();
 
 	//assert
+	ASSERT_TRUE(true);
 
 }
 
+TEST(QueueTests, TestIfQueueIsNotFull) {
+	Queue* q = new Queue();
+	q->enqueue(2);
+	q->enqueue(3);
+	q->enqueue(4);
+	q->enqueue(5);
+	
+
+	//act
+	q->isFull();
+
+	//assert
+	ASSERT_FALSE(false);
+
+}
 
 TEST(QueueTests, TestEnqueue) {
 	//set
+	Queue* q = new Queue();
 
 	//act
-
+	q->enqueue(2);
+	int size = q->count();
+	
 	//assert
-
+	ASSERT_EQ(size, 1);
 }
 
 
 TEST(QueueTests, TestDequeue) {
 	//set
+	Queue* q = new Queue();
 
 	//act
+	q->enqueue(2);
+	int dequeueVal = q->dequeue();
 
 	//assert
+	ASSERT_EQ(dequeueVal, 2);
 
 }
 
 TEST(QueueTests, TestCountOfElementsInArray) {
-	//set
+	Queue* q = new Queue();
 
 	//act
+	q->enqueue(2);
+	q->enqueue(3);
+	q->enqueue(4);
+	int size = q->count();
 
 	//assert
-
+	ASSERT_EQ(size, 3);
 }
 
 TEST(QueueTests, TestFrontElementInQueue) {
-	//set
+	Queue* q = new Queue();
 
 	//act
+	q->enqueue(2);
+	q->enqueue(3);
+	q->enqueue(4);
 
+	int frontVal = q->front();
 	//assert
+
+	ASSERT_EQ(frontVal, 2);
 
 }
